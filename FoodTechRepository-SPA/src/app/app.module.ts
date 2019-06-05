2d667c5e4b40c5af5d38e537a3fcbad0e39fbf03
Sonali Rokade <sonali.rokade@nitorinfotech.net>
2019-06-05 17:07:42 +0530

    HttpClient Added

--- FoodTechRepository-SPA/src/app/app.module.ts
+++ FoodTechRepository-SPA/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
